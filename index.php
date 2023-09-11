f70d972752a2463aba4893fbf8ebeafbfaea61c8
itstrazu <donesbyron05@gmail.com>
2023-09-11 21:06:43 +0800

    changed the file type to .php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory System</title>
+</head>
+<body>
+    <header class="das_header">
+        <h1>Dones Auto Supply Inventory System</h1>
+    </header>
+
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="das_insert_product.php">Insert Product</a></li>
+        <li><a href="das_search_product.php">Search Product</a></li>
+        <li><a href="das_update_product.php">Update Product</a></li>
+        <li><a href="das_delete_product.php">Delete Product</a></li>
+        <li class="left_logout_1"><a href="das_login_page.html">Logout</a></li>
+    </ul>
+
+    <ul class="left_logout">
+
+    </ul>
+
+    <footer>
+        <hr>
+        All Rights Reserved, 2023
+    </footer>
+</body>
+</html>
